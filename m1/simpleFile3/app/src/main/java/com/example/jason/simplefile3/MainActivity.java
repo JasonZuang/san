26b8e18a218ec785729554973cc526d16ca30208
jason <zuangjason@gmail.com>
2017-02-01 23:02:49 -0600

    as

--- m1/simpleFile3/app/src/main/java/com/example/jason/simplefile3/MainActivity.java
+++ m1/simpleFile3/app/src/main/java/com/example/jason/simplefile3/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.jason.simplefile3;
 
+import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+/*
+    I had help from some online rescources
+    http://stackoverflow.com/questions/9544737/read-file-from-assets
+    http://stackoverflow.com/questions/30417810/reading-from-a-text-file-in-android-studio-java
+    https://www.tutorialspoint.com/java/java_files_io.htm
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        File file = new File(Environment.getExternalStorageDirectory(),"new.txt");
+        BufferedReader is = null;
+        FileWriter os;
+        try{
+            is = new BufferedReader( new InputStreamReader (getAssets().open("smalltext.txt"),"UTF-8"));
+            os = new FileWriter(file);
+            String a ="";
+            while((a = is.readLine()) != null ) {
+                System.out.println(a);//prints to debugger
+                os.write(a);
+            }
+            os.close();
+            file.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(is != null)
+            {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
